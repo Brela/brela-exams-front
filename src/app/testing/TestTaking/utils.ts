9dda272eea43e9537a21d657ffa9d1dbcdf0204f
Brela <claybreland1@gmail.com>
2024-05-30 07:45:14 -0500

    removed testing page content for now

--- src/app/testing/TestTaking/utils.ts
+++
@@ -1,4 +0,0 @@
-export const getLetterOption = (index: number) => {
-  const letterKey = ["A", "B", "C", "D"];
-  return letterKey[index];
-};
--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,50 +1,9 @@
 'use client';
 
-import './home.css';
-import { useState } from 'react';
-import { Button } from '@mantine/core';
-import TestTaking from './TestTaking/Test';
-import './TestTaking/test.css';
-
-export type Stats = {
-  score: number;
-  numOfQuestions: number;
-};
-
 function Home() {
-  const [testInProgress, setTestInProgress] = useState(false);
-  const [calculatedScore, setCalculatedScore] = useState<number | null>(null);
-  const closeTest = (stats: Stats) => {
-    setTestInProgress(false);
-
-    if (stats) {
-      const percentage = (100 / stats.numOfQuestions) * stats.score;
-      setCalculatedScore(percentage);
-    }
-  };
-
   return (
     <>
-      <div className="home-container">
-        <>
-          <h2>{`Breland's Guessing Game`}</h2>
-          {testInProgress ? (
-            <TestTaking closeTest={closeTest} />
-          ) : (
-            <Button
-              onClick={() => {
-                setTestInProgress(true);
-                setCalculatedScore(null);
-              }}
-            >
-              start test
-            </Button>
-          )}
-          {calculatedScore && (
-            <div>{`You scored ${calculatedScore}% of questions correct! Great job!`}</div>
-          )}
-        </>
-      </div>
+      <div className="">Test page</div>
     </>
   );
 }
